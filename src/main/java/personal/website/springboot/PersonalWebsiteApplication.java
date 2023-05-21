0f1c3870e14c799352cc4041b328ba5eec4bb8cf
BogdanSaf <210117691@aston.ac.uk>
2023-05-21 14:37:57 +0100

    Adding Project

---
+++ src/main/java/personal/website/springboot/PersonalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package personal.website.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebsiteApplication.class, args);
+	}
+
+}
